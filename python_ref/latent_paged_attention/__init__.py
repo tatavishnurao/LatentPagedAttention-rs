692b077bb058eba3e4dd1d6335ef2c08f9f1f323
tatavishnurao <tatavishnurao@gmail.com>
2026-07-09 22:09:02 +0530

    Export paged latent KV reference helpers

--- python_ref/latent_paged_attention/__init__.py
+++ python_ref/latent_paged_attention/__init__.py
@@ -3,9 +3,12 @@
 from .attention_ref import (
     gqa_decode_attention_ref,
     latent_kv_decode_attention_ref,
+    paged_gqa_decode_attention_ref,
+    paged_latent_kv_decode_attention_ref,
     paged_lookup_ref,
+    softmax_stable,
 )
-from .block_table import PagedBlockTable
+from .block_table import PagedBlockTable, logical_blocks_for_seq_len
 from .memory_model import (
     compression_ratio,
     estimate_total_kv_cache_bytes,
@@ -21,5 +24,9 @@ __all__ = [
     "kv_bytes_per_token_gqa",
     "kv_bytes_per_token_latent",
     "latent_kv_decode_attention_ref",
+    "logical_blocks_for_seq_len",
+    "paged_gqa_decode_attention_ref",
+    "paged_latent_kv_decode_attention_ref",
     "paged_lookup_ref",
+    "softmax_stable",
 ]
